68dd799c8b91e37066243e1aaff41d0dba702e4e
Meh1928 <thejedi64@gmail.com>
2022-06-14 10:29:44 +0800

    Jump

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -7,7 +7,7 @@ using UnityEngine;
 public class PlayerBehaviour : MonoBehaviour
 {
     public float speed = 5f;
-    public float jumpForce = 1f;
+    public float jumpForce = 5f;
 
     private Rigidbody2D rb;
     private void Start()
@@ -23,7 +23,7 @@ public class PlayerBehaviour : MonoBehaviour
     void Update()
     {
         float xMove = Input.GetAxisRaw("Horizontal");
-        rb.velocity = new Vector2(xMove, rb.velocity.y) * speed;
+        transform.Translate(new Vector3(xMove, 0, 0) * (speed * Time.deltaTime));
 
         if (Input.GetKeyDown(KeyCode.Space))
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
