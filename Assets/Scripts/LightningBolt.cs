cebf37587026933198050073937dafe51c13b3e2
Meh1928 <thejedi64@gmail.com>
2022-06-22 00:01:27 +0800

    even more tweakin

--- Assets/Scripts/LightningBolt.cs
+++ Assets/Scripts/LightningBolt.cs
@@ -38,7 +38,7 @@ public class LightningBolt : MonoBehaviour
     {
         tint = c;
         alpha = 1.5f;
-        fadeSpeed = 0.001f;
+        fadeSpeed = 0.01f;
         
         //Create bolt
         if (Vector2.Distance(end, start) <= 0)
