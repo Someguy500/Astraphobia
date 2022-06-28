42fa1c06b9dabf1860928d94f9e78c09dc511c93
Meh1928 <92201578+Someguy500@users.noreply.github.com>
2022-06-28 23:25:11 +0800

    Parallax Tweak (Ter)
    still not 100% right, but getting closer

--- Assets/Scripts/ParallaxScript.cs
+++ Assets/Scripts/ParallaxScript.cs
@@ -1,10 +1,14 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ParallaxScript : MonoBehaviour
 {
 private float length, startpos;
+
+[SerializeField] private float scaleMult;
 
 public GameObject cam;
 
@@ -15,20 +19,21 @@ public float parallaxEffect;
         startpos = transform.position.x;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
-    
+
     void Update()
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + dist, transform.position.y);
 
-        if (temp > startpos + length)
+        if (cam.transform.position.x > startpos + 3*length)
         {
-            startpos += length;
+            startpos += 3*length;
+            
         }
-        else if (temp < startpos - length)
+        else if (cam.transform.position.x < startpos - 3*length)
         {
-            startpos -= length;
+            startpos -= 3*length;
         }
     }
 }
