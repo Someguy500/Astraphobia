4a93899868b88a873f6da50726918b68ed005758
AaronTan1 <0129389@kdu-online.com>
2022-07-03 01:29:45 +0800

    interactable featued updated
    -fixed joint 2d bug fixed

--- Assets/Scripts/MovableScript.cs
+++ Assets/Scripts/MovableScript.cs
@@ -4,14 +4,14 @@ using UnityEngine;
 
 public class MovableScript : MonoBehaviour
 {
-    public float distance = 0.01f;
+    public float distance = 1f;
     public LayerMask boxMask;
-    GameObject box;
+    bool onPush = true;
 
-    // Start is called before the first frame update
-    void Start()
+    GameObject box;
+    private void Start()
     {
-        
+        Debug.Log(box);
     }
 
     // Update is called once per frame
@@ -19,13 +19,34 @@ public class MovableScript : MonoBehaviour
     {
         Physics2D.queriesStartInColliders = false;
         RaycastHit2D hit = Physics2D.Raycast(transform.position, Vector2.right * transform.localScale.x, distance, boxMask);
-        if(hit.collider != null && Input.GetKey(KeyCode.V))
-        {
-            box = hit.collider.gameObject;
 
-            box.GetComponent<FixedJoint2D>().enabled = true;
-            box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+        if (onPush)
+        {
+            if (hit.collider != null && Input.GetKeyDown(KeyCode.V))
+            {
+                box = hit.collider.gameObject;
+
+                box.GetComponent<FixedJoint2D>().enabled = true;
+                box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
+                Debug.Log(box);
+                Debug.Log("attached 1 ^");
+                onPush = false;
+            }
         }
+        else
+        {
+            if (hit.collider == null && Input.GetKeyDown(KeyCode.V))
+            {
+
+                box.GetComponent<FixedJoint2D>().enabled = false;
+                Debug.Log(box);
+                Debug.Log("attached 1 ^");
+                onPush = true;
+            }
+        }
+
+        
+
     }
 
     private void OnDrawGizmos()
