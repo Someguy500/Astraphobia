7d2a22920c7f16b8207500536cce4ba067138891
Meh1928 <92201578+Someguy500@users.noreply.github.com>
2022-07-04 21:11:20 +0800

    Basic Checkpoint (Ter)
    checkpoint records player position on touch, player will be placed back on recorded position instead of start

--- Assets/Scripts/PlayerFall.cs
+++ Assets/Scripts/PlayerFall.cs
@@ -7,10 +7,13 @@ public class PlayerFall : MonoBehaviour
     float fixedPosY = -7.06f;
     private bool isDead = false;
     public static Vector2 origin;
+    public Rigidbody2D rb;
+    [SerializeField] private LayerMask CPLayerMask;
 
     void Start()
     {
         origin = transform.position;
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void fallDeath()
@@ -33,6 +36,13 @@ public class PlayerFall : MonoBehaviour
         {
             isDead = true;
             fallDeath();
+        }
+        
+        RaycastHit2D CheckpointCheck = Physics2D.Raycast(new Vector2(rb.position.x, rb.position.y), Vector2.down, 1, CPLayerMask);
+
+        if (CheckpointCheck.collider != null)
+        {
+            origin = transform.position;
         }
     }
 }
