a429a43afb3884b629204014a8e390ce34f6e808
Meh1928 <92201578+Someguy500@users.noreply.github.com>
2022-07-13 00:46:25 +0800

    Sound Manager Beta, Volume Slider

---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class VolumeManager : MonoBehaviour
+{
+    private static float Vol;
+    private bool pendingChange = true;
+    private AudioListener audListen;
+
+    void Update()
+    {
+        
+    }
+    
+}
